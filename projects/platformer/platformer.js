b1918753f00d6f5b06143be18ef70c5b80473618
DylanD85 <dylan.davis@carvercollegiate.org>
2024-09-09 14:10:36 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,10 +38,10 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(350,250,200,20)
+    createPlatform(1050,250,200,20)
+    createPlatform(1050,500,200,20)
+    createPlatform(700,0,200,20)
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
